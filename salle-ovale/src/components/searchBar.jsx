29c21b457d099b4fc58987fd8293a1b63207ef79
Dam-C <dams.c128@gmail.com>
2024-02-05 18:23:47 +0100

    deploy test

--- salle-ovale/src/components/searchBar.jsx
+++ salle-ovale/src/components/searchBar.jsx
@@ -7,7 +7,10 @@ const SearchBar = ({ search, dispatch }) => {
   const handleQuerySubmit = async (event) => {
     event.preventDefault();
     axios
-      .post("http://127.0.0.1:5000/search", { userSearch: search })
+      // .post("http://127.0.0.1:5000/search", { userSearch: search })
+      .post("https://so-books-search-back.vercel.app/search", {
+        userSearch: search,
+      })
       .then((response) => {
         const content = response.data;
         if (Object.prototype.hasOwnProperty.call(content, "noResult")) {
